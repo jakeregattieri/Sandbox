4c5bee461af04eddcf6a9268bcdcc7dc826c9f1b
jakeregattieri <jake.regattieri@my.jcu.edu.au>
2017-03-13 11:12:05 +1000

    If applied, this commit will add name printing program.

--- oddName.py
+++ oddName.py
@@ -1,1 +1,10 @@
 """Jake"""
+INVALID_NAME = ""
+
+
+user_name = input("what is your name: ".strip())
+while user_name == INVALID_NAME:
+    print("invalid name")
+    user_name = input("what is your name: ".strip())
+for chr in user_name[::2]:
+    print(chr)
